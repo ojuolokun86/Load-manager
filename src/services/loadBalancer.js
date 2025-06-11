8c2b848562fda6a872a6ec33ee4183694cde776a
ojuolokun86 <ojuolokun86@gmail.com>
2025-06-11 22:15:38 +0100

    new load

--- src/services/loadBalancer.js
+++ src/services/loadBalancer.js
@@ -9,7 +9,7 @@ export async function assignServerForUser() {
   // Always try VPS first, up to 50 bots
   const vps = botServers.find(s => s.id === 'VPS');
   const vpsStatus = status['VPS'];
-  if (vps && vpsStatus?.healthy && (vpsStatus.load || 0) < 50) {
+  if (vps && vpsStatus?.healthy && (vpsStatus.load || 0) < 7) {
     console.log(`[LOAD BALANCER] Assigned server: VPS`);
     return 'VPS';
   }
